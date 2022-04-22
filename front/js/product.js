c0c274c1943af230ef4330caf1db9e415e526312
beautythieves <96109098+beautythieves@users.noreply.github.com>
2022-04-22 08:41:42 +0200

    prix

--- front/js/product.js
+++ front/js/product.js
@@ -12,4 +12,34 @@ fetch('http://localhost:3000/api/products/' + idKanap)/*appel de l'api*/
 
   function createProduct(monCanape) {
     console.log(monCanape);
-  }
+
+    /*insertion de la photo du canapé*/
+      let photo = document.createElement("img");
+      document.querySelector(".item__img").appendChild(photo);
+      photo.src = monCanape.imageUrl;
+      photo.alt = monCanape.alt;
+      console.log('photo', photo);
+    
+      /*insertion du nom du canapé*/
+      let nomDuCanape = document.createElement("h1");
+      document.querySelector("#title").appendChild(nomDuCanape);
+      nomDuCanape.innerText = monCanape.name;
+      console.log( "nom", nomDuCanape);
+
+      /*insertion du prix du canapé*/
+      let prixDuCanape = document.createElement("p");
+      document.querySelector ("#price").appendChild(prixDuCanape);
+      prixDuCanape.innerText = monCanape.price;
+      console.log ("prix", prixDuCanape );
+
+      /*insertion du descriptif du canapé*/
+      let DescriptifCanape = document.getElementById("description");
+      DescriptifCanape.innerHTML = monCanape.description;
+
+      /*choix de la couleur
+      for (let couleurs of )
+      {
+
+      }
+      console.log ("coloro", couleurs)*/
+    }
