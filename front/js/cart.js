a94ea43cc3f28d6e7c62b69d4d41db63231a5251
beautythieves <96109098+beautythieves@users.noreply.github.com>
2022-05-03 17:59:05 +0200

    cart js

--- front/js/cart.js
+++ front/js/cart.js
@@ -20,20 +20,65 @@ else {
         /*insertion DIV image produit*/
         let divImage = document.createElement("div");
         document.querySelector("article").appendChild(divImage);
-        divImage.classname = "cart__item__img";
+        divImage.classList.add("cart__item__img");
         console.log("divimage", divImage);
 
-        /*insertion image canapé*/
+        /*insertion image canapé l.53 (cf.cart.html)*/
         let photoCanape = document.createElement("img");
         divImage.appendChild(photoCanape);
         photoCanape.src = monPanier[i].imageUrl;
         console.log("photo du canapé", photoCanape);
         /*!!pb ci dessus insertion image?*/
 
-        let contentItem = document.createElement("div");
-        
+        /*insertion div l.55 à l.70(cf cart.html)*/
+        let cartItemContent = document.createElement("div");
+        articleProduit.appendChild(cartItemContent);
+        cartItemContent.classList.add("cart__item__content");
+        console.log("cart item", cartItemContent);
 
+        /*insertion div l.56 à l.60 (cf cart.html)*/
+        let cartItemContentDescription = document.createElement("div");
+        cartItemContent.appendChild(cartItemContentDescription);
+        cartItemContentDescription.classList.add("cart__item__content__description");
+        console.log("div56 à 60", cartItemContentDescription);
         
+        /*insertion h2 nom du produit(cf l.57 cart.html)*/
+        let nomDuProduit = document.createElement("h2");
+        cartItemContentDescription.appendChild(nomDuProduit);
+        nomDuProduit.innerText = monPanier[i].name;
+        console.log("nom", nomDuProduit);
+        
+        /* insertion de la couleur (balise p, l.58 cart.html)*/
+        let couleurDuProduit = document.createElement("p");
+        cartItemContentDescription.appendChild(couleurDuProduit);
+        couleurDuProduit.innerText = monPanier[i].color;
+        console.log("coloro", couleurDuProduit);
 
+        /*insertion de la balise p "prix", l.59 cart.html*/
+        let prixDuProduit = document.createElement("p");
+        cartItemContentDescription.appendChild(prixDuProduit);
+        prixDuProduit.innerText = monPanier[i].prixDuCanape;
+        console.log("atouprix!", prixDuProduit);
+
+        /*insertion de la balise div l.61 à l.69 cart.html*/
+        let cartItemContentSettings = document.createElement("div");
+        cartItemContent.appendChild(cartItemContentSettings);
+        cartItemContentSettings.classList.add("cart__item__content__settings");
+        console.log("lalala", cartItemContentSettings);
+
+        /*insertion de la balise div l.62 à l.65 cart.html*/
+        let cartItemContentSettingsQuantity = document.createElement("div");
+        cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
+        cartItemContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
+        console.log("ketafé", cartItemContentSettingsQuantity);
+
+        /*insertion de la balise p "Qté", l.63 cart.html*/
+        let quantité = document.createElement("p");
+        cartItemContentSettingsQuantity.appendChild(quantité);
+        quantité.innerText = "Qté :";
+
+        /*insertion de la balise input quantité, l.64 cart.html*/
+        
+    
     }
 }
