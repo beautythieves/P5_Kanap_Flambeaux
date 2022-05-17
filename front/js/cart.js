1073e790dab6519974b1b4968f665ed1e578cc69
beautythieves <96109098+beautythieves@users.noreply.github.com>
2022-05-17 15:47:50 +0200

    pb fetch

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,13 +8,10 @@ function main() {
 
 function displayCanaps(monPanier) {
     //clear tous les articles HTML présent sur la page!!!!!!!!!!!
-    let suppressionArticles = document.getElementsByClassName("cart__item");
-    console.log(suppressionArticles);
-    //tableaux d'éléments, queryselector all section puis vider*/
-    suppressionArticles.remove;
-    /*cidessus, marche pas*/
-    /*message d'alerte si le panier est vide*/
+    let suppressionArticles = document.querySelector("#cart__items");
+    suppressionArticles.innerHTML = '';
 
+    /*message d'alerte si le panier est vide*/
     if (monPanier == null || monPanier.length == 0) {
         alert ("Votre panier est vide. Veuillez choisir un ou plusieurs article(s)")
     }
@@ -28,71 +25,58 @@ function displayCanaps(monPanier) {
                 //J'ajoute le prix et la quantité
                 quantity += monPanier[i].quantity;
                 price += data.price;
-                console.log('quantity', quantity)
                 /*création de la balise article dans la section*/
                 let articleProduit = document.createElement("article");
                 document.querySelector("#cart__items").appendChild(articleProduit);
-                console.log ("article", articleProduit);
                 articleProduit.className = "cart__item";
                 articleProduit.dataset.id = monPanier[i].id;
                 articleProduit.dataset.color = monPanier[i].color;
-                console.log ("article", articleProduit);
 
                 /*insertion DIV image produit*/
                 let divImage = document.createElement("div");
                 articleProduit.appendChild(divImage);
                 divImage.classList.add("cart__item__img");
-                console.log("divimage", divImage);
 
                 /*insertion image canapé l.53 (cf.cart.html)*/
                 let photoCanape = document.createElement("img");
                 divImage.appendChild(photoCanape);
                 photoCanape.src = data.imageUrl;
                 photoCanape.alt = "Photographie d'un canapé";
-                console.log("photo du canapé", photoCanape);
-                /*!!pb ci dessus insertion image?*/
 
                 /*insertion div l.55 à l.70(cf cart.html)*/
                 let cartItemContent = document.createElement("div");
                 articleProduit.appendChild(cartItemContent);
                 cartItemContent.classList.add("cart__item__content");
-                console.log("cart item", cartItemContent);
 
                 /*insertion div l.56 à l.60 (cf cart.html)*/
                 let cartItemContentDescription = document.createElement("div");
                 cartItemContent.appendChild(cartItemContentDescription);
                 cartItemContentDescription.classList.add("cart__item__content__description");
-                console.log("div56 à 60", cartItemContentDescription);
                 
                 /*insertion h2 nom du produit(cf l.57 cart.html)*/
                 let nomDuProduit = document.createElement("h2");
                 cartItemContentDescription.appendChild(nomDuProduit);
                 nomDuProduit.innerText = data.name;
-                console.log("nom", nomDuProduit);
                 
                 /* insertion de la couleur (balise p, l.58 cart.html)*/
                 let couleurDuProduit = document.createElement("p");
                 cartItemContentDescription.appendChild(couleurDuProduit);
                 couleurDuProduit.innerText = monPanier[i].color;
-                console.log("coloro", couleurDuProduit);
 
                 /*insertion de la balise p "prix", l.59 cart.html*/
                 let prixDuProduit = document.createElement("p");
                 cartItemContentDescription.appendChild(prixDuProduit);
                 prixDuProduit.innerText = data.price * monPanier[i].quantity;
-                console.log("atouprix!", prixDuProduit);
 
                 /*insertion de la balise div l.61 à l.69 cart.html*/
                 let cartItemContentSettings = document.createElement("div");
                 cartItemContent.appendChild(cartItemContentSettings);
                 cartItemContentSettings.classList.add("cart__item__content__settings");
-                console.log("lalala", cartItemContentSettings);
 
                 /*insertion de la balise div l.62 à l.65 cart.html*/
                 let cartItemContentSettingsQuantity = document.createElement("div");
                 cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
                 cartItemContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
-                console.log("ketafé", cartItemContentSettingsQuantity);
 
                 /*insertion de la balise p "Qté", l.63 cart.html*/
                 let quantité = document.createElement("p");
@@ -109,7 +93,7 @@ function displayCanaps(monPanier) {
                 input.max="100";
                 input.value= monPanier[i].quantity;
                 input.onchange = function() {
-                    updateQuantity(input.value, data.id, monPanier[i].color);      
+                    updateQuantity(input.value, data._id, monPanier[i].color);      
                 }  
 
                 /*insertion de la balise div l.66 à l;68 cart.html*/
@@ -134,7 +118,6 @@ function displayCanaps(monPanier) {
 }
 
 function removeFromCart(color, id) {
-    console.log('je supprime larticle');   
     let monPanier = JSON.parse(localStorage.getItem("monPanier")); 
      /*boucle pour suprimer le(s) article(s*/
     for (let i = 0; i < monPanier.length; i++) {      
@@ -149,14 +132,16 @@ function removeFromCart(color, id) {
 } 
 
 function updateQuantity(newQuantity, idKanap, colorKanap) {
-    console.log('je change la quantite'); 
+    console.log('mouchard1'); 
     let monPanier = JSON.parse(localStorage.getItem("monPanier")); 
-    console.log("monpanier", monPanier);
-    
-    for (let i = 0; i < monPanier.length; i++) {      
+    console.log('mouchard2'); 
+    for (let i = 0; i < monPanier.length; i++) {  
+        console.log('bouclefor: mouchard1');
+        console.log('ifr: mouchard1'); 
         if (monPanier[i].id === idKanap && monPanier[i].color == colorKanap) {
+            console.log('ifr: mouchard1');  
             monPanier[i].quantity = newQuantity;
-            console.log("monPanier", monPanier); 
+            console.log('ifr: mouchard2');
         }
     }
     localStorage.setItem("monPanier", JSON.stringify(monPanier));
@@ -184,31 +169,63 @@ function setFormValidation() {
     incorrectement, message d'erreur*/
 
     boutonValidationCommande.addEventListener("click", function() {
+        /*console.log('on test le prenom', prenom, regexPrenom.test(prenom));
         if (regexPrenom.test(prenom) === false){
             alert ("Veuillez saisir un prénom");
         }
-        if (regexNom.test(nom) == false){
+        if (regexNom.test(nom) === false){
             alert ("Veuillez saisir un nom");
         }
-        if (regexAdresse.test(adresse) == false){
+        if (regexAdresse.test(adresse) === false){
             alert ("Veuillez saisir une adresse valide");
         }
-        if (regexEmail.test(email) == false){
-            alert ("Veuillez saisir une adresse email valide");
-        } else {
-            fetch('', {
+        if (regexVille.test(ville) === false){
+            alert ("Veuillez saisir un nom de ville valide");
+        }
+        if (regexEmail.test(email) === false){
+            alert ("Veuillez saisir une adresse email valide");*/
+        //} else {
+            /*Pour les routes POST, l’objet contact envoyé au serveur 
+            doit contenir les champs firstName,lastName, address, city et email. 
+            Le tableau des produits envoyé au back-end doit être un array de 
+            strings product-ID. Les types de ces champs et leur présence
+             doivent être validés avant l’envoi des données au serveur*/
+            
+            let monPanier = JSON.parse(localStorage.getItem("monPanier"));
+            
+            let products = [];
+            for (let i = 0; i < monPanier.length; i++) {
+                //pour chaque objet du panier, je met son id dans mon tableau products
+                products.push(monPanier[i].id);
+            }
+
+            /*création objet contact*/
+            let order = {
+                contact : {
+                  firstName : prenom,
+                  lastName : document.getElementById("lastName").value,
+                  address : document.getElementById("address").value,
+                  city : document.getElementById("city").value,
+                  email : document.getElementById("email").value
+                },
+                products: products,
+            }
+            console.log ("contact", order);
+            fetch('http://localhost:3000/api/products/order', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: {firstname: ''},
-            }).then((data) => {
+                body: JSON.stringify(order),
+                }).then((data) => {
                 //le data doit contenir l'id de la commande
-                console.log(data)
+                console.log('voici la réponse de ma requete', data)
                 //redirigera vers la page confirmation en passant l'id dans l'url
-            });
-        }
+                }).catch((err) => {
+                    console.log('error', err);
+                });
+        //}
     });
 }
 
