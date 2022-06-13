5e2744022867058e06977c5b07612a215f8c1b3c
beautythieves <96109098+beautythieves@users.noreply.github.com>
2022-06-13 09:51:29 +0200

    throw conso log

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -5,11 +5,8 @@ function principale() {
 }
 
 function ChargementIdCommande() {
-    console.log (window.location);
     let searchParams = new URLSearchParams(window.location.search);
-    console.log(searchParams);
     let UrlCommandeId = new URL(location.href).searchParams.get("orderId");
-    console.log ("order", UrlCommandeId);
     let numeroCommande = document.querySelector("#orderId");
     orderId.textContent = UrlCommandeId;
 }
